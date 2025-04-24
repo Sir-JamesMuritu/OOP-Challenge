3d27ca13198e1590050599bbb755410583593f5c
JAMES MURITU <jemo7158@gmail.com>
2025-04-24 11:36:28 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
+from pet import Pet
+
+# Create a pet
+pet = Pet("Bosco")
+print(f"Creating pet: {pet.name} 🐾")
+
+pet.eat()
+pet.play()
+pet.sleep()
+pet.train("roll over")
+pet.train("play dead")
+
+pet.get_status()
+
+pet.show_tricks()
 
